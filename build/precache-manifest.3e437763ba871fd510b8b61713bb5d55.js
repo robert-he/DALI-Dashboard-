00fdcbe12cde70cb66ac130e962cc38eb7b1f5a6
Robert He <roberthe@Roberts-MacBook-Pro-6.local>
2018-12-14 21:38:52 -0500

    build folder

---
+++ build/precache-manifest.3e437763ba871fd510b8b61713bb5d55.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "50a0a7b26783bd19f3a9",
+    "url": "/static/css/main.421be943.chunk.css"
+  },
+  {
+    "revision": "50a0a7b26783bd19f3a9",
+    "url": "/static/js/main.50a0a7b2.chunk.js"
+  },
+  {
+    "revision": "854658b4fd9053442bdf",
+    "url": "/static/js/1.854658b4.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "ed709ce8c8db74db4e6dddac649b8142",
+    "url": "/index.html"
+  }
+];
